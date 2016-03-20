0f0792c48df9c2ceac1c220fdd1e154b9c55481b
Tristan Daniel <Tristan Daniel>
2016-03-20 17:48:28 +0100

    Redirection des fichiers statiques pour le serveur PHP interne.

--- index.php
+++ index.php
@@ -2,21 +2,30 @@
 
 define('ROOT', __DIR__);
 
-switch ($_SERVER['REQUEST_URI'])
+$uri = $_SERVER['REQUEST_URI'];
+
+if (preg_match('/.*\\.(css|js|png|jpg|jpeg|mp3|ogg|woff|woff2|ttf)$/i', $uri) === 1)
 {
-	case '/':
-		echo 'Index';
-		break;
-
-	case '/electre1':
-		require ROOT.'/electre/electre1.php';
-		break;
-
-	case '/application':
-		require ROOT.'/application.html';
-		break;
-
-	default:
-		echo '404';
-		break;
+	header('Content-Type: text/css');
+	readfile($uri, true);
 }
+else
+	switch ($uri)
+	{
+		case '/':
+			echo 'Index';
+			break;
+
+		case '/electre1':
+			require ROOT.'/electre/electre1.php';
+			break;
+
+		case '/application':
+			require ROOT.'/application.html';
+			break;
+
+		default:
+			http_response_code(404);
+			echo '404 not found';
+			break;
+	}
